ab6dc70fb4f6c71fbe26c323bbbc4927c5bf44d3
Crowdbotics <team@crowdbotics.com>
2023-11-02 10:51:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "reversed-44287",
+  url: "https://reversed-44287.botics.co",
+  api: "https://reversed-44287.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
